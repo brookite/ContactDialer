67df38cf3cb4e23a0bd51f3c69cdcf276b548d55
Brookit <brookit18@gmail.com>
2021-12-30 21:11:12 +0300

    Extended search and open all cards by button

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, QtGui
 import sys
 from ui.mainview import Ui_MainWindow as MainViewUI
 from ui.contactview import Ui_Form as ContactViewUI
+from ui.finddialog import Ui_Form as FindDialogUI
 from core import AppCore
 from core.search import SearchEngine
 
@@ -20,6 +21,80 @@ class ContactView(QtWidgets.QWidget):
         self._ui.email.setText("Email:" + ";".join(map(str, self._contact.email.values)))
 
 
+class FindDialog(QtWidgets.QWidget):
+    VIEW_CMP = {
+        "equal": "Equal",
+        "contain": "Contain",
+        "fuzzy": "Fuzzy",
+        "end": "At end",
+        "start": "At start",
+        "all": "All",
+        "default": "Default"
+    }
+
+    APP_CMP = {
+        "Equal": "equal",
+        "Contain": "contain",
+        "Fuzzy": "fuzzy",
+        "At end": "end",
+        "At start": "start",
+        "All": "all",
+        "Default": "default"
+    }
+
+    def __init__(self, mainview, searcher):
+        super().__init__()
+        self._searcher = searcher
+        self._mainview = mainview
+        self._ui = FindDialogUI()
+        self._ui.setupUi(self)
+        self.restore()
+        self.setWindowTitle("Search")
+        self._ui.find.clicked.connect(self.find)
+        self._ui.applySettings.clicked.connect(self.apply_settings)
+        self._ui.clearSearch.clicked.connect(self.clear)
+
+    def app_representation(self, text):
+        return self.APP_CMP[text]
+
+    def view_representation(self, text):
+        return self.VIEW_CMP[text]
+
+    def restore(self):
+        self._ui.searchtype.setCurrentText(self.view_representation(self._searcher.settings["type"]))
+        self._ui.casechk.setChecked(self._searcher.settings["case"])
+        self._ui.phonemethod.setCurrentText(self.view_representation(self._searcher.settings["phone_constraints"]))
+        self._ui.fuzzy_k.setValue(self._searcher.settings["fuzzy_k"])
+        targets = self._searcher.settings["objects"]
+        if "phone" in targets:
+            self._ui.phone.setChecked(True)
+        if "name" in targets:
+            self._ui.name.setChecked(True)
+        if "email" in targets:
+            self._ui.email.setChecked(True)
+
+    def clear(self):
+        self._mainview.find("")
+
+    def apply_settings(self):
+        targets = []
+        if self._ui.phone.isChecked():
+            targets.append("phone")
+        if self._ui.name.isChecked():
+            targets.append("name")
+        if self._ui.email.isChecked():
+            targets.append("email")
+        self._searcher.set_parameters(objects=targets, 
+            type=self.app_representation(self._ui.searchtype.currentText()),
+            case=self._ui.casechk.isChecked(),
+            phone_constraints=self.app_representation(self._ui.phonemethod.currentText()),
+            fuzzy_k=self._ui.fuzzy_k.value()
+        )
+    
+    def find(self):
+        self.apply_settings()
+        self._mainview.find(self._ui.mainSearch.text())
+
 class MainView(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -29,12 +104,16 @@ class MainView(QtWidgets.QMainWindow):
         self._ui.setupUi(self)
         self._ui.open_file.triggered.connect(self.open_filedialog)
         self._ui.openVcards.clicked.connect(self.open_contact)
+        self._ui.actionFind.triggered.connect(self.open_search)
+        self._ui.actionOpen_all_cards.triggered.connect(self.openAll)
         self._contentLayout = QtWidgets.QVBoxLayout(self._ui.content)
         self._view = None
+        self.fd = None
         self._searcher = SearchEngine(self._core._vcards, self._core._indexer)
         self._model = QtGui.QStandardItemModel()
         self._ui.openVcards.setModel(self._model)
         self._ui.openVcards.doubleClicked.connect(self.checkUnopened)
+        self._ui.searchSettings.clicked.connect(self.open_search)
         self.openInitialVcards()
         self._ui.searchField.editingFinished.connect(self.search_field_signal)
         self.setWindowTitle("ContactDialer")
@@ -46,6 +125,11 @@ class MainView(QtWidgets.QMainWindow):
 
     def search_field_signal(self):
         self.find(self._ui.searchField.text())
+
+    def open_search(self):
+        if not self.fd:
+            self.fd = FindDialog(self, self._searcher)
+        self.fd.show()
 
     def find(self, query):
         if query.strip():
@@ -61,13 +145,24 @@ class MainView(QtWidgets.QMainWindow):
             self._core.revert_tree()
             self.refresh_tree(self._core._tree)
 
+    def openAll(self):
+        for i in range(self._model.rowCount()):
+            path = self._model.item(i)
+            if self._core.is_unloaded(path.text()):
+                self._core.fetch_vcard(self._core._vcards[path.text()][1])
+                self.refresh_tree(self._core.vcard_tree(path.text()))
+
     def checkUnopened(self, i):
         path = self._model.item(i.row())
         if path:
-            path = path.text()
-            if self._core.is_unloaded(path):
-                self._core.fetch_vcard(self._core._vcards[path][1])
-                self.refresh_tree(self._core.vcard_tree(path))
+            if self._core.is_unloaded(path.text()):
+                self._core.fetch_vcard(self._core._vcards[path.text()][1])
+                self.refresh_tree(self._core.vcard_tree(path.text()))
+            path = self._model.item(i.row())
+            if not self._ui.openVcards.isExpanded(path.index()):
+                self._ui.openVcards.setExpanded(path.index(), True)
+            else:
+                self._ui.openVcards.setExpanded(path.index(), False)
 
     def open_filedialog(self):
         # Returns ui representation of contact bundle
